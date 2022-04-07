d3d4348bc453b4b446247f71e315c4d865b40353
rkutaraev@list.ru <rkutaraev@list.ru>
2022-04-07 10:17:56 +0300

    Добавьте файлы проекта.

---
+++ Lesson_work5/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_work5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
