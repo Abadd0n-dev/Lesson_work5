f361d62cabe0ac4e4ec881a1c8e488a3c11bdbee
rkutaraev@list.ru <rkutaraev@list.ru>
2022-04-07 10:20:10 +0300

    Ввести с клавиатуры произвольный набор данных и сохранить его в текстовый файл.

--- Lesson_work5/Program.cs
+++ Lesson_work5/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,35 @@ namespace Lesson_work5
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Введите расположение файла");
+            var path = Console.ReadLine();
+            if (string.IsNullOrEmpty(path) || string.IsNullOrWhiteSpace(path))
+            {
+                Console.WriteLine("Неверно указанный путь");
+                return;
+            }
+
+            if (!path.EndsWith(".txt")) path += ".txt";
+            var fs = new FileStream(path, File.Exists(path) ? FileMode.Append : FileMode.OpenOrCreate);
+            var sw = new StreamWriter(fs);
+
+
+            Console.WriteLine("Введите данные для текстового файла, нажмите esc, чтобы остановиться");
+            sw.AutoFlush = true;
+            while (true)
+            {
+                var input = Console.ReadKey();
+
+                if (input.Key == ConsoleKey.Escape)
+                    break;
+                if (input.Key == ConsoleKey.Enter)
+                {
+                    sw.Write("\n");
+                    Console.WriteLine();
+                }
+                else sw.Write(input.KeyChar);
+            }
+            Console.WriteLine("Записанно");
         }
     }
 }
