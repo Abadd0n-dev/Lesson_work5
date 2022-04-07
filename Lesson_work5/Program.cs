f40878cf4bd9d9596311160d4439cbcca8a47cc4
rkutaraev@list.ru <rkutaraev@list.ru>
2022-04-07 10:23:52 +0300

    Ввести с клавиатуры произвольный набор чисел (0...255) и записать их в бинарный файл.

--- Lesson_work5/Program.cs
+++ Lesson_work5/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +12,40 @@ namespace Lesson_work5
     {
         static void Main(string[] args)
         {
+            const string file = "test.bin";
+
+            Console.WriteLine("Введите числа от [0..255], через пробел");
+            var input = Console.ReadLine()?.Split(' ');
+
+            var array = new byte[input.Length];
+            for (var i = 0; i < array.Length; i++)
+            {
+                if (!byte.TryParse(input[i], out var number))
+                {
+                    Console.WriteLine($"Неверное число {input[i]}");
+                    return;
+                }
+
+                array[i] = number;
+            }
+
+            using (var bw = new BinaryWriter(File.OpenWrite(file)))
+            {
+                bw.Write(array);
+                bw.Flush();
+            }
+
+            Console.WriteLine($"в файле сейчас находится {file}"); ; ;
+            byte[] newData;
+            using (var br = new BinaryReader(File.OpenRead(file)))
+            {
+                newData = br.ReadBytes(array.Length);
+            }
+
+            for (var i = 0; i < newData.Length; i++)
+            {
+                Console.Write(newData[i] + " ");
+            }
         }
     }
 }
